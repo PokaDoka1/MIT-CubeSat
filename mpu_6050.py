20c7343119e07552202262400086788efb16d930
PokaDoka1 <90730806+PokaDoka1@users.noreply.github.com>
2022-04-27 19:19:14 -0700

    Update mpu_6050.py

--- mpu_6050.py
+++ mpu_6050.py
@@ -1,5 +1,7 @@
 from mpu6050 import mpu6050
 import time
+from git import Repo
+from picamera import PiCamera
 mpu = mpu6050(0x28)
 
 while True:
@@ -7,15 +9,30 @@ while True:
     print()
 
     accel_data = mpu.get_accel_data()
-    print("Acc X : "+str(accel_data['x']))
+    """print("Acc X : "+str(accel_data['x']))
     print("Acc Y : "+str(accel_data['y']))
     print("Acc Z : "+str(accel_data['z']))
-    print()
+    print()"""
 
     gyro_data = mpu.get_gyro_data()
-    print("Gyro X : "+str(gyro_data['x']))
+    """print("Gyro X : "+str(gyro_data['x']))
     print("Gyro Y : "+str(gyro_data['y']))
     print("Gyro Z : "+str(gyro_data['z']))
     print()
-    print("-------------------------------")
+    print("-------------------------------")"""
     time.sleep(1)
+
+def git_push():
+    try:
+        #halps
+        repo = Repo('/home/pi/Home/MIT-CubeSat')  # PATH TO YOUR GITHUB REPO
+        #halps
+        repo.git.add('IMU Data')  # PATH TO YOUR IMAGES FOLDER WITHIN YOUR GITHUB REPO
+        repo.index.commit('Gyro Data')
+        print('made the commit')
+        origin = repo.remote('origin')
+        print('added remote')
+        origin.push()
+        print('pushed changes')
+    except:
+        print('Couldnt upload to git')
