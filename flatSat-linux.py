1ced6588bde0a6f66f9133038253049fbbcdf68d
PokaDoka1 <90730806+PokaDoka1@users.noreply.github.com>
2022-05-22 18:00:00 -0700

    Update flatSat-linux.py

--- flatSat-linux.py
+++ flatSat-linux.py
@@ -68,6 +68,7 @@ while True:
                 t=time.strftime("_%H%M%S") #current time string
                 imgname = ('/home/cubesat/Home/MIT-CubeSat/Images/blah') #chagne didrectory to your folder
                 img = cv2.imread(camera.capture(imgname+ ".jpg"))
+                print("the type is " + str(type(img)))
                 for color in green:
                     if diff + color > 255:
                          dog.append(255)
